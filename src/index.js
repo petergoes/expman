798f2d508cb2ec9a70922698fb095ead911d3745
Peter Goes <petergoes@gmail.com>
2015-03-16 23:05:25 +0100

    basic overview layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 angular.module('app', ['ngMaterial']);
+
+angular.module('app')
+	.controller('AppController', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
+		$scope.toggleSidenav = function(menuId) {
+			$mdSidenav(menuId).toggle();
+		};
+	}]);
+
+angular.module('app')
+	.controller('ItemsController', ['$scope', function($scope){
+		$scope.items = [
+			{
+				title: "Item 1",
+				amount: 3.95
+			},
+			{
+				title: "Item 2",
+				amount: 3.95
+			},
+			{
+				title: "Item 3",
+				amount: 3.95
+			}
+		];
+	}]);
