f8abf88909644b13d52f69f585a5efbcd62addf0
Peter Goes <petergoes@gmail.com>
2015-03-16 22:03:52 +0100

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+	'use strict';
+
+	grunt.initConfig({
+		copy: {
+			dev: {
+				files: [
+					{expand: true, cwd: 'src/', src: ['index.html'], dest: 'dev/'},
+
+					{expand: true, cwd: 'src/', src: ['**/*.js'], dest: 'dev/'},
+
+					{expand: true, cwd: 'bower_components/angularjs/',			src: ['angular.min.js', 'angular.min.js.map'], dest: 'dev/vendor'},
+					{expand: true, cwd: 'bower_components/angular-animate', 	src: ['angular-animate.min.js', 'angular-animate.min.js.map'], dest: 'dev/vendor'},
+					{expand: true, cwd: 'bower_components/angular-aria',		src: ['angular-aria.min.js', 'angular-aria.min.js.map'], dest: 'dev/vendor'},
+					{expand: true, cwd: 'bower_components/angular-material',	src: ['angular-material.min.js', 'angular-material.min.css'], dest: 'dev/vendor'}
+				]
+			}
+		},
+		less: {
+			dev: {
+				options: {
+					compress: false,
+					sourceMap: true,
+					sourceMapFileInline: true
+				},
+				files: {
+					'dev/styles.css': 'src/index.less'
+				}
+			}
+		},
+		notify: {
+			dev: {
+				options: {
+					title: 'ExpMan',  // optional
+					message: 'Files copied' //required
+				}
+			}
+		},
+		watch: {
+			js: {
+				files: ['src/**/*'],
+				tasks: ['dev', 'notify:dev']
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-notify');
+
+	grunt.registerTask('dev', ['copy:dev', 'less:dev']);
+};
