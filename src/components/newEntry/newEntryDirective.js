06fed826f6b273b1dd8a522652eeb5b1ce774af7
Peter Goes <petergoes@gmail.com>
2015-03-28 16:51:33 +0100

    put newEntry in own controller

---
+++ src/components/newEntry/newEntryDirective.js
@@ -0,0 +1,33 @@
+(function() {
+	'use strict';
+
+	angular
+		.module('app')
+		.directive('newEntry', NewEntry);
+
+	/* @ngInject */
+	function NewEntry ($templateCache) {
+		// Usage:
+		//
+		// Creates:
+		//
+		var directive = {
+			bindToController: true,
+			template: template,
+			controller: 'NewEntryController',
+			controllerAs: 'newEntry',
+			link: link,
+			restrict: 'EA',
+			scope: {
+			}
+		};
+		return directive;
+
+		function link(scope, element, attrs) {
+		}
+
+		function template() {
+			return $templateCache.get('src/components/newEntry/newEntry.html');
+		}
+	}
+})();
