96d1777794b28df41599791b8a61b03413b6976b
Peter Goes <petergoes@gmail.com>
2015-03-29 16:33:34 +0200

    add events so primaryFab can be (de)activated and clicked

--- src/components/newEntry/newEntryController.js
+++ src/components/newEntry/newEntryController.js
@@ -6,22 +6,42 @@
 		.controller('NewEntryController', NewEntry);
 
 	/* @ngInject */
-	function NewEntry($scope) {
+	function NewEntry($scope, $rootScope) {
 		var vm = this;
-		vm.title = 'NewEntry';
 		vm.amount = null;
+		vm.canSubmit = false;
 		vm.description = '';
 		vm.tags = [
 			{name: 'Food', used: 2, checked: false},
 			{name: 'Home', used: 1, checked: false},
 			{name: 'Other', used: 1, checked: false},
 			{name: 'Sports', used: 5, checked: false}];
+		vm.title = 'NewEntry';
 		vm.toggleMoreTags = false;
 		vm.toggleTags = toggleTags;
 
 		init();
 
+		$scope.$watch('newEntry.amount', amountChanged);
+		$scope.$watch('newEntry.canSubmit', canSubmitChanged);
+
 		////////////////
+
+		function amountChanged(newValue) {
+			if (newValue > 0) {
+				vm.canSubmit = true;
+			} else {
+				vm.canSubmit = false;
+			}
+		}
+
+		function canSubmitChanged(canSubmit) {
+			if (canSubmit) {
+				$rootScope.$broadcast('primary-fab-activate');
+			} else {
+				$rootScope.$broadcast('primary-fab-deactivate');
+			}
+		}
 
 		function init() {
 		}
--- src/components/primaryFab/primaryFabController.js
+++ src/components/primaryFab/primaryFabController.js
@@ -6,15 +6,35 @@
 		.controller('PrimaryFabController', PrimaryFabController);
 
 	/* @ngInject */
-	function PrimaryFabController() {
+	function PrimaryFabController($scope, $rootScope) {
 		var vm = this;
+		vm.clicked = clicked;
+		vm.disabled = true;
 		vm.title = 'PrimaryFabController';
 
-		activate();
+		$scope.$on('primary-fab-activate', activate);
+		$scope.$on('primary-fab-deactivate', deactivate);
+		$scope.$on('$stateChangeSuccess', deactivate);
+
+		init();
 
 		////////////////
 
+		function clicked() {
+			$rootScope.$broadcast('primary-fab-clicked');
+		}
+
 		function activate() {
+			console.log('activate');
+			vm.disabled = false;
+		}
+
+		function deactivate() {
+			console.log('deactivate');
+			vm.disabled = true;
+		}
+
+		function init() {
 		}
 	}
 })();
